7506aaccb4020cefe9cf99bc3d6feeb9517c3eae
Mamczyk <146847796+Mamczyk@users.noreply.github.com>
2023-10-11 17:56:35 +0200

    Add files via upload

---
+++ cwiczenia/lab1.cs
@@ -0,0 +1,19 @@
+﻿namespace cwiczenia
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            p12();
+        }
+        static void p12()
+        {
+            Console.Write("Podaj promień: ");
+            double r = Double.Parse(Console.ReadLine());
+            double obwod = 2 * Math.PI * r;
+            Console.WriteLine("Obwód wynosi: {0:0.##}", obwod);
+        }
+    }
+
+}
